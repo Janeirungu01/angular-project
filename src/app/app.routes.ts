8e726ea0dc6c882150337093bf606587be2dfe4f
Janeirungu01 <jwirungu96@gmail.com>
2025-08-15 17:04:59 +0300

    test routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { AboutUs } from './about-us/about-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: App},
+    {path: 'about-us', component: AboutUs}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-greeting',
+  selector: 'app-greetings',
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
    name: string = 'Jane Doe';
    today: Date = new Date();
- 
 }
 
 
